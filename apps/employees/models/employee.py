8350d400ca85aefaa3c2f90b1fb50342a80c5478
mqudah0031 <71003640+mqudah0031@users.noreply.github.com>
2021-10-23 20:12:08 +0300

    test

--- apps/employees/models/employee.py
+++ apps/employees/models/employee.py
@@ -1,7 +1,9 @@
 from sqlalchemy import Boolean, Column, String, UniqueConstraint
 
 from sqlalchemy.orm import column_property
+
 from core.db import Base
+
 from apps.business.models import SubDomainRef
 
 
